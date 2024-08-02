c6d786877e7c958cd195a98b9101454d8e1d5ae3
Leticia Nobre <leticiavidal2910@gmail.com>
2024-08-01 21:26:13 -0300

    🔧 refat: update tailwind config file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
-import type { Config } from "tailwindcss";
+// tailwind.config.js
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -8,13 +9,16 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Work Sans', 'sans-serif'],
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
   plugins: [],
 };
+
 export default config;
