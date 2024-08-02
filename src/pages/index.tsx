cd340e16e517b1ef8dc2ad32b559e9a53d345829
Leticia Nobre <leticiavidal2910@gmail.com>
2024-08-01 22:11:59 -0300

    ✨ feat: added about page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,14 @@
 import { Inter } from 'next/font/google';
-import ThemeToggleButton from '@/components/ThemeButton';
-import Navbar from '@/components/Navbar';
+import Initial from '@/components/Initial';
+import About from '@/components/About';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
     <main className={`flex min-h-screen flex-col items-center justify-between font-sans ${inter.className}`}>
-      <Navbar/>
+      <Initial/>
+      <About/>
     </main>
   );
 }
