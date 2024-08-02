10563438af4e52524d26d539e1f59a9096d139e6
Leticia Nobre <leticiavidal2910@gmail.com>
2024-08-01 22:11:03 -0300

    ♻️ refat: adjusted  home layout

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -4,8 +4,8 @@ const inter = Inter({ subsets: ['latin'] });
 
 export default function Navbar() {
   return (
-      <header className='w-full flex flex-row py-4 px-20 items-center justify-between'>
-        <h1>DEVELOPER</h1>
+      <header className='w-full h-16 flex flex-row py-4 px-20 items-center justify-between'>
+        <h1 className="text-gradient font-extrabold text-2xl">DEVELOPER</h1>
         <div className='flex flex-row justify-between items-center gap-6'>
           <ThemeToggleButton />
           <button>Home</button>
