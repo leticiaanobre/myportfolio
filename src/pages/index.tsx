641d58116f4c88c983417d50c594deb5f092b926
Leticia Nobre <leticiavidal2910@gmail.com>
2024-08-01 21:29:57 -0300

    🌱 feat: added navbar to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,13 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
+import { Inter } from 'next/font/google';
+import ThemeToggleButton from '@/components/ThemeButton';
+import Navbar from '@/components/Navbar';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <h1>Text</h1>
+    <main className={`flex min-h-screen flex-col items-center justify-between font-sans ${inter.className}`}>
+      <Navbar/>
     </main>
   );
 }
