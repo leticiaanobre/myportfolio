b699a97f09095a3b8d3f583432add36acbcf5b97
Leticia Nobre <leticiavidal2910@gmail.com>
2024-08-01 21:30:30 -0300

    🌱 refat: refact  app and document files

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,30 @@
-import { Html, Head, Main, NextScript } from "next/document";
+// pages/_document.tsx
+import { Html, Head, Main, NextScript } from 'next/document';
+
+function setInitialColorMode() {
+  const colorMode = window.localStorage.getItem('color-mode') || 'dark';
+  document.documentElement.setAttribute('data-theme', colorMode);
+}
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@400;500;700&display=swap" rel="stylesheet" />
+      </Head>
       <body>
         <Main />
         <NextScript />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function() {
+                const colorMode = window.localStorage.getItem('color-mode') || 'light';
+                document.documentElement.setAttribute('data-theme', colorMode);
+              })();
+            `,
+          }}
+        />
       </body>
     </Html>
   );
