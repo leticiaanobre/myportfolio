dd3997e1f3b5cb48039ba371a2f2f8ed2a164b17
Leticia Nobre <leticiavidal2910@gmail.com>
2024-08-03 10:04:49 -0300

    ♻️ refat: home page

--- src/components/Initial/index.tsx
+++ src/components/Initial/index.tsx
@@ -1,12 +1,18 @@
+import { ChevronDown } from "lucide-react";
 import Navbar from "../Navbar";
 
 export default function Initial() {
   return (
     <section className="w-full min-h-screen flex flex-col">
       <Navbar />
-      <div className="flex-grow flex flex-col gap-4 items-center justify-center">
-        <h1 className='text-4xl font-sans'>I'm a Frontend Developer specialising in</h1>
-        <h1 className='text-4xl font-sans'><strong>UX/UI</strong> and <strong>Interaction Design</strong></h1>
+      <div className="flex-grow flex flex-col gap-4 items-center justify-evenly">
+        <div className="flex flex-col gap-4 items-center justify-center">
+            <h1 className='text-4xl font-sans'>I'm a Frontend Developer specialising in</h1>
+            <h1 className='text-4xl font-sans'><strong>UX/UI</strong> and <strong>Interaction Design</strong></h1>
+        </div>
+        <div>
+        <ChevronDown />
+        </div>
       </div>
     </section>
   );
