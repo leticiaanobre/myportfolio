9d85f8916790e9f3f2c17f4125bb8ce426add9e5
Leticia Nobre <leticiavidal2910@gmail.com>
2024-08-03 10:04:18 -0300

    ✨ feat: added about page

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,6 +1,16 @@
 
 export default function About () {
     return(
-        <h1>About</h1>
+        <section className="w-full min-h-screen flex flex-col">
+            <div className="flex-grow flex flex-row gap-4 items-center justify-center">
+                <div className='w-2/5 flex flex-col gap-4'>
+                    <h1 className='text-2xl'>A BIT ABOUT ME</h1>
+                    <p className="font-thin text-3xl"> I am a UI/UX designer who is passionate about creating beautiful and joyful digital experiences. Besides design, I love music, games and travelling. </p>
+                </div>
+                <div className='w-2/5 flex items-center justify-center'>
+                    <p>Thre images here</p>
+                </div>
+            </div>
+        </section>
     )
 }
