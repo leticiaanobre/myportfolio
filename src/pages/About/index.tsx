baf4e8c7b334dfe11146f5937d6105f15a2bf527
Leticia Nobre <leticiavidal2910@gmail.com>
2024-08-03 11:32:28 -0300

    💄 style: adjusted about me font

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -4,8 +4,8 @@ export default function About () {
         <section className="w-full min-h-screen flex flex-col">
             <div className="flex-grow flex flex-row gap-4 items-center justify-center">
                 <div className='w-2/5 flex flex-col gap-4'>
-                    <h1 className='text-2xl'>A BIT ABOUT ME</h1>
-                    <p className="font-thin text-3xl"> I am a UI/UX designer who is passionate about creating beautiful and joyful digital experiences. Besides design, I love music, games and travelling. </p>
+                    <h1 className='text-1xl'>A BIT ABOUT ME</h1>
+                    <p className="font-thin text-2xl leading-relaxed"> I am a UI/UX designer who is passionate about creating beautiful and joyful digital experiences. Besides design, I love music, games and travelling. </p>
                 </div>
                 <div className='w-2/5 flex items-center justify-center'>
                     <p>Thre images here</p>
