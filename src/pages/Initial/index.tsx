93febeccb86cc327161b73867bb797a50e0673d4
Leticia Nobre <leticiavidal2910@gmail.com>
2024-08-07 15:27:13 -0300

    💄 style: adjusted font weight

--- src/pages/Initial/index.tsx
+++ src/pages/Initial/index.tsx
@@ -7,8 +7,8 @@ export default function Initial() {
       <Navbar />
       <div className="flex-grow flex flex-col gap-4 items-center justify-evenly">
         <div className="flex flex-col gap-4 items-center justify-center">
-            <h1 className='text-4xl font-sans'>I'm a Frontend Developer specialising in</h1>
-            <h1 className='text-4xl font-sans'><strong>UX/UI</strong> and <strong>Interaction Design</strong></h1>
+            <p className='font-extralight text-4xl leading-relaxed'>I'm a Frontend Developer specialising in</p>
+            <p className='font-extralight text-4xl leading-relaxed'><strong>UX/UI</strong> and <strong>Interaction Design</strong></p>
         </div>
         <button>
         <ChevronDown />
--- src/pages/Projects/index.tsx
+++ src/pages/Projects/index.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 export default function Projects() {
   return (
     <section className="w-full min-h-screen px-24 py-16 gap-10 flex flex-col">
-      <p className="font-thin text-2xl">
+      <p className="font-extralight text-2xl">
         Selected <strong>Projects</strong>
       </p>
       <div className="flex flex-row gap-14 mt-8">
@@ -18,23 +18,23 @@ export default function Projects() {
           />
         </div>
         <div className="flex flex-col gap-4 justify-end items-start">
-          <h1 className="text-1xl">REAL ESTATE TEMPLATE</h1>
-          <p className="font-thin text-2xl leading-relaxed">
+          <h1 className="text-1xl font-bold text-gradient">REAL ESTATE TEMPLATE</h1>
+          <p className="font-extralight text-2xl leading-relaxed">
             Lorem ipsum dolor sit amet, consectetur{' '}
             <strong>adipiscing elit. Ultrices lorem non feugiat egestas amet.</strong>
           </p>
-          <p className="font-thin">Website Design</p>
+          <p className="font-extralight">Website Design</p>
           <button className="flex flex-row text-sm items-center border py-1 px-6 rounded-full">VIEW WORK <ChevronRight className="ml-2"/></button>
         </div>
       </div>
       <div className="flex flex-row gap-14 mt-8">
         <div className="flex flex-col gap-4 justify-end items-start">
-          <h1 className="text-1xl">REAL ESTATE TEMPLATE</h1>
-          <p className="font-thin text-2xl leading-relaxed">
+          <h1 className="text- font-bold text-gradient">REAL ESTATE TEMPLATE</h1>
+          <p className="font-extralight text-2xl leading-relaxed">
             Lorem ipsum dolor sit amet, consectetur{' '}
             <strong>adipiscing elit. Ultrices lorem non feugiat egestas amet.</strong>
           </p>
-          <p className="font-thin">Website Design</p>
+          <p className="font-extralight">Website Design</p>
           <button className="flex flex-row text-sm items-center border py-1 px-6 rounded-full">VIEW WORK <ChevronRight className="ml-2"/></button>
         </div>
         <div className="relative w-[700px] h-[350px]">
@@ -58,12 +58,12 @@ export default function Projects() {
           />
         </div>
         <div className="flex flex-col gap-4 justify-end items-start">
-          <h1 className="text-1xl">REAL ESTATE TEMPLATE</h1>
-          <p className="font-thin text-2xl leading-relaxed">
+          <h1 className="text-1xl font-bold text-gradient">REAL ESTATE TEMPLATE</h1>
+          <p className="font-extralight text-2xl leading-relaxed">
             Lorem ipsum dolor sit amet, consectetur{' '}
             <strong>adipiscing elit. Ultrices lorem non feugiat egestas amet.</strong>
           </p>
-          <p className="font-thin">Website Design</p>
+          <p className="font-extralight">Website Design</p>
           <button className="flex flex-row text-sm items-center border py-1 px-6 rounded-full">VIEW WORK <ChevronRight className="ml-2"/></button>
         </div>
       </div>
