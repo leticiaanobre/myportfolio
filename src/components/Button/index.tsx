496473000739b0b697e157fa53be4d1bfe04cd88
Leticia Nobre <leticiavidal2910@gmail.com>
2024-08-07 19:55:50 -0300

    ✨ feat: created buttonComponent

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,35 @@
+import Link, { LinkProps } from "next/link";
+import { LucideIcon } from 'lucide-react';
+
+interface ButtonComponentProps {
+    text: string;
+    icon?: LucideIcon;
+    link?: LinkProps["href"];
+}
+
+const ButtonComponent: React.FC<ButtonComponentProps> = ({ text, icon: Icon, link }) => {
+    const content = (
+        <>
+            {text}
+            {Icon && <Icon className="inline-block" />}
+        </>
+    );
+
+    const baseClass = "flex flex-row text-sm items-center border py-1 px-6 rounded-full gap-2 hover:bg-customPurple transition duration-300 group";
+
+    if (link) {
+        return (
+            <Link href={link}>
+                <a className={baseClass}>{content}</a>
+            </Link>
+        );
+    }
+
+    return (
+        <button className={baseClass}>
+            {content}
+        </button>
+    );
+}
+
+export default ButtonComponent;
