2db38ff699446a7a0aae01654781fa0a55fcf522
Leticia Nobre <leticiavidal2910@gmail.com>
2024-08-07 15:25:55 -0300

    ✨ feat: added footer component

---
+++ src/pages/Footer/index.tsx
@@ -0,0 +1,22 @@
+import { Github, Linkedin, Mail, MessageCircle } from "lucide-react";
+
+
+export default function Footer() {
+  return (
+    <section className="w-full h-64 flex flex-col">
+      <footer className="flex-grow flex flex-col gap-2 items-center justify-evenly">
+        <div className="flex flex-col gap-4 items-center justify-center">
+            <p className="font-extralight text-4xl leading-relaxed">Get <strong> in touch</strong></p>
+            <p className="font-extralight text-1xl leading-relaxed">So then we can talk more about</p>
+        </div>
+        <div className='flex flex-row gap-10'>
+            <Mail />
+            <Github />
+            <Linkedin />
+            <MessageCircle />
+        </div>
+        <p className="font-thin text-1xl leading-relaxed">Made by <strong> Leticia Nobre</strong></p>
+      </footer>
+    </section>
+  );
+}
