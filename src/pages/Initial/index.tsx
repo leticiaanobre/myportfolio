fe72e164511dbda7b3551ff8151cf97820e79555
Leticia Nobre <leticiavidal2910@gmail.com>
2024-08-08 19:18:17 -0300

    💚 refat: refat code to fit build enviroment

--- src/pages/Initial/index.tsx
+++ src/pages/Initial/index.tsx
@@ -7,7 +7,7 @@ export default function Initial() {
       <Navbar />
       <div className="flex-grow flex flex-col gap-4 items-center justify-evenly">
         <div className="flex flex-col gap-4 items-center justify-center">
-            <p className='font-extralight text-4xl leading-relaxed'>I'm a Frontend Developer specialising in</p>
+            <p className='font-extralight text-4xl leading-relaxed'>I&apos;m a Frontend Developer specialising in</p>
             <p className='font-extralight text-4xl leading-relaxed'><strong>UX/UI</strong> and <strong>Interaction Design</strong></p>
         </div>
         <button>
