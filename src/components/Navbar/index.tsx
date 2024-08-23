71ed53bf5ae4b483dd996c977c33d14fc1855107
Leticia Nobre <leticiavidal2910@gmail.com>
2024-08-23 19:29:44 -0300

    💄 style: adjusted navbar buttons layout

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -2,11 +2,13 @@ import { useState } from 'react';
 import { Inter } from 'next/font/google';
 import ThemeToggleButton from '@/components/ThemeButton';
 import { Menu, X } from 'lucide-react';
+import { useColorMode } from '../../hook/useColorMode';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const { colorMode, toggleColorMode } = useColorMode();
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
@@ -15,28 +17,41 @@ export default function Navbar() {
   return (
     <header className="w-full h-16 flex flex-row py-4 px-6 md:px-20 items-center justify-between">
       <h1 className="text-gradient font-extrabold text-2xl">DEVELOPER</h1>
-      <div className="flex flex-row gap-2 md:hidden">
-        <ThemeToggleButton />
-        <button onClick={toggleMenu} className="text-gray-800 dark:text-gray-200">
-          {isMenuOpen ? <X className="h-6 w-6 text-color" /> : <Menu className="h-6 w-6 text-color" />}
-        </button>
-      </div>
-      <div className="hidden md:flex items-center gap-6">
-        <button>Home</button>
-        <button>About</button>
-        <button>Projects</button>
-      </div>
+      <div className='flex flex-grow justify-end gap-2'>
+        <div className="flex flex-row gap-2">
+          <ThemeToggleButton/>
+          <button onClick={toggleMenu} className="text-gray-800 dark:text-gray-200 md:hidden">
+            {isMenuOpen ? <X className="h-6 w-6 text-color" /> : <Menu className="h-6 w-6 text-color" />}
+          </button>
+        </div>
+        <div className="hidden md:flex items-center gap-4">
+          <button className={`py-2 px-3 rounded-xl transition duration-300 ${
+          colorMode === 'light' ? 'hover:bg-gray-200' : 'hover:bg-gray-700'
+        }`}>Home</button>
+          <button className={`py-2 px-3 rounded-xl transition duration-300 ${
+          colorMode === 'light' ? 'hover:bg-gray-200' : 'hover:bg-gray-700'
+        }`}>About</button>
+          <button className={`py-2 px-3 rounded-xl transition duration-300 ${
+          colorMode === 'light' ? 'hover:bg-gray-200' : 'hover:bg-gray-700'
+        }`}>Projects</button>
+        </div>
 
       {isMenuOpen && (
         <div className="absolute top-16 left-0 w-full bg-background bg-color shadow-lg md:hidden">
           <div className="flex flex-col items-center py-4">
-            <button className="py-2">Home</button>
-            <button className="py-2">About</button>
-            <button className="py-2">Projects</button>
-            <ThemeToggleButton />
+            <button className={`py-3 w-full ${
+          colorMode === 'light' ? 'hover:bg-gray-200' : 'hover:bg-gray-700'
+        }`}>Home</button>
+            <button className={`py-3 w-full ${
+          colorMode === 'light' ? 'hover:bg-gray-200' : 'hover:bg-gray-700'
+        }`}>About</button>
+            <button className={`py-3 w-full ${
+          colorMode === 'light' ? 'hover:bg-gray-200' : 'hover:bg-gray-700'
+        }`}>Projects</button>
           </div>
         </div>
       )}
+      </div>
     </header>
   );
 }
