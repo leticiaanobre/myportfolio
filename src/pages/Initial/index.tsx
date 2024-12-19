3f81c361a6040880730d00138d1ca62eef58dedd
Leticia Nobre <leticiavidal2910@gmail.com>
2024-12-19 16:05:14 -0300

    refat: description

--- src/pages/Initial/index.tsx
+++ src/pages/Initial/index.tsx
@@ -8,7 +8,7 @@ export default function Initial() {
       <Navbar />
       <div className="flex-grow flex flex-col gap-4 items-center justify-evenly p-10">
         {/* Image Section */}
-        <div className="relative flex items-center justify-center animate-bounce-slow">
+        <div className="relative flex items-center justify-center">
           <div className="w-48 h-48 sm:w-60 sm:h-60 md:w-72 md:h-72 rounded-full overflow-hidden">
             <Image
               src="/LETICIADARK1.jpg" 
@@ -22,10 +22,10 @@ export default function Initial() {
         {/* Text Section */}
         <div className="flex flex-col gap-4 items-center justify-center">
           <p className="text-center font-extralight text-2xl md:text-4xl leading-relaxed">
-            I&apos;m a Frontend Developer specialising in
+            I&apos;m a <strong>Software Developer</strong> with a passion
           </p>
           <p className="text-center font-extralight text-2xl md:text-4xl leading-relaxed">
-            <strong>UX/UI</strong> and <strong>Interaction Design</strong>
+          for building efficient <strong>web applications</strong>
           </p>
         </div>
         <a href="#about">
