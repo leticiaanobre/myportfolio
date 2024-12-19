907113759bc638528466224b9a58d06ccf84b018
Leticia Nobre <leticiavidal2910@gmail.com>
2024-12-19 19:48:31 -0300

    feat: added image domain

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['localhost', 'leticianobreportfolio.vercel.app'],
+  },
 };
 
 export default nextConfig;
