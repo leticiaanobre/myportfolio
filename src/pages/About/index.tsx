554465bc812598d275df7e36585b943df02f071b
Leticia <leticiavidal2910@gmail.com>
2024-12-29 20:26:10 -0300

    feat: added new colors

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -6,7 +6,7 @@ export default function About() {
       <div className="flex-grow flex flex-col md:flex-row gap-4 items-center justify-center p-10">
         {/* Texto */}
         <div className="w-full md:w-2/5 flex flex-col gap-4">
-          <h1 className="text-1xl font-medium">A BIT ABOUT ME</h1>
+          <h1 className="text-1xl font-bold text-gradient">A BIT ABOUT ME</h1>
           <p className="font-extralight text-base md:text-2xl leading-relaxed">
           I am currently a Computer Science student. I work as a <strong>software developer</strong> with a focus on <strong>web</strong> and <strong>mobile development</strong>, utilizing technologies like Node.js, ReactJS, and React Native. My journey in the tech world has been driven by curiosity and a passion for building efficient and user-centered applications.
           </p>
