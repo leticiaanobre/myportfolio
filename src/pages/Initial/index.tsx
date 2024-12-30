05ccb143ce3874aaa55dc208219815c8a6d552d7
Leticia <leticiavidal2910@gmail.com>
2024-12-29 22:10:09 -0300

    refat: changed layout to hero page

--- src/pages/Initial/index.tsx
+++ src/pages/Initial/index.tsx
@@ -1,42 +1,43 @@
-import { ChevronDown } from "lucide-react";
-import Navbar from "../../components/Navbar";
-import Image from "next/image";
+import Image from "next/image"
+import Navbar from "@/components/Navbar"
+import { ChevronDown } from "lucide-react"
 
-export default function Initial() {
+export default function Home() {
   return (
-    <section className="w-full min-h-screen flex flex-col">
+    <div className="min-h-screen bg-gray-950">
       <Navbar />
-      <div className="flex-grow flex flex-col md:flex-row gap-4 items-center justify-evenly p-10">
-        {/* Image Section */}
-        <div className=" flex items-center justify-center">
-          <div className="w-48 h-48 sm:w-60 sm:h-60 md:w-96 md:h-96 rounded-full overflow-hidden">
-            <Image
-              src="/profileSmile.jpeg"
-              alt="Image of myself"
-              width={488}
-              height={488}
-              className="object-cover"
-            />
-          </div>
+      <section className="container min-h-screen grid lg:grid-cols-2 gap-8 items-center pt-16">
+        <div className="relative aspect-square w-full max-w-2xl mx-auto">
+          <Image
+            src="/LETICIADARK1.JPG"
+            alt="Portrait photo"
+            fill
+            className="object-cover rounded-sm"
+            priority
+          />
         </div>
-        {/* Text Section */}
-        <div className="flex flex-col gap-4 items-center justify-center md:items-start md:justify-start">
-          <p className="text-center md:text-start font-semibold text-lg md:text-xl leading-relaxed">Hi, there!</p>
-          <p className="text-center md:text-start font-extralight text-2xl md:text-4xl leading-relaxed">
-            I&apos;m a <strong className="text-gradient font-semibold">Software Developer</strong> with a passion
-          </p>
-          <p className="text-center md:text-start font-extralight text-2xl md:text-4xl leading-relaxed">
-            for building efficient <strong className="text-gradient font-semibold">web applications</strong>
+        <div className="space-y-6 px-8">
+          <div className="space-y-2">
+            <h1 className="text-4xl sm:text-5xl md:text-5xl font-medium text-white">
+              I&apos;m Leticia Nobre.
+              <br />
+              A <strong className="font-bold text-gradient">Software Developer</strong>
+            </h1>
+            <p className="text-4xl sm:text-5xl md:text-5xl font-medium">
+              based in Brazil.
+            </p>
+          </div>
+          <p className="text-base md:text-xl font-extralight max-w-2xl">
+            I&apos;m passionate about building efficient web applications.
           </p>
           <a href="#about">
-            <button className="flex gap-2 bg-blue-600 items-center py-2 px-4 hover:bg-blue-700 rounded-lg">
-              See more
-              <ChevronDown />
-            </button>
-          </a>
+          <button className="animate-bounce mt-4">
+            <ChevronDown />
+          </button>
+        </a>
         </div>
-
-      </div>
-    </section>
-  );
+      </section>
+    </div>
+  )
 }
+
