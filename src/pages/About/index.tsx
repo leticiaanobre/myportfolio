8113d3de4459b8656b9e0521b239679f91b7d1bf
Leticia <leticiavidal2910@gmail.com>
2025-01-03 22:17:03 -0300

    feat: added animation to skills

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,14 +1,23 @@
 import Image from "next/image";
+import useIntersectionObserver from "@/hook/intersectionObserver";
 
 export default function About() {
+  const { isVisible, ref } = useIntersectionObserver<HTMLDivElement>();
+
   return (
-    <section className="w-full min-h-screen flex flex-col" id="about">
+    <section
+      ref={ref}
+      className={`w-full min-h-screen flex flex-col ${isVisible ? 'translate-y-0 opacity-100' : 'translate-y-10 opacity-0'} 
+        transform transition-all duration-700 ease-in-out`}
+      id="about"
+    >
       <div className="flex-grow flex flex-col md:flex-row gap-4 items-center justify-center p-10">
         {/* Texto */}
         <div className="w-full md:w-2/5 flex flex-col gap-4">
           <h1 className="text-1xl font-bold text-gradient">A BIT ABOUT ME</h1>
           <p className="font-extralight text-base md:text-xl leading-relaxed ">
-          I am currently a Computer Science student. I work as a <strong>software developer</strong> with a focus on <strong>web</strong> and <strong>mobile development</strong>, utilizing technologies like Node.js, ReactJS, and React Native. My journey in the tech world has been driven by curiosity and a passion for building efficient and user-centered applications.
+            I am currently a Computer Science student. I work as a <strong>software developer</strong> with a focus on{" "}
+            <strong>web</strong> and <strong>mobile development</strong>, utilizing technologies like Node.js, ReactJS, and React Native. My journey in the tech world has been driven by curiosity and a passion for building efficient and user-centered applications.
           </p>
         </div>
 
