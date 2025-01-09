0cbd1eaadfa31c09610aef62a2079c61d580db7f
Leticia <leticiavidal2910@gmail.com>
2025-01-09 20:09:12 -0300

    refat: adjusted style and text

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -11,13 +11,13 @@ export default function About() {
         transform transition-all duration-700 ease-in-out`}
       id="about"
     >
-      <div className="flex-grow flex flex-col md:flex-row gap-4 items-center justify-center p-10">
+      <div className="flex-grow flex flex-col md:flex-row gap-4 items-center justify-center p-10 bg-radial">
         {/* Texto */}
         <div className="w-full md:w-2/5 flex flex-col gap-4">
-          <h1 className="text-1xl font-bold text-gradient">A BIT ABOUT ME</h1>
+          <h1 className="text-1xl font-bold">A BIT ABOUT ME</h1>
           <p className="font-extralight text-base md:text-xl leading-relaxed ">
-            I am currently a Computer Science student. I work as a <strong>software developer</strong> with a focus on{" "}
-            <strong>web</strong> and <strong>mobile development</strong>, utilizing technologies like Node.js, ReactJS, and React Native. My journey in the tech world has been driven by curiosity and a passion for building efficient and user-centered applications.
+            I am currently a Computer Science student. I work as a <strong className="text-primary font-semibold">software developer</strong> with a focus on{" "}
+            <strong className="text-primary font-semibold">web</strong> and <strong className="text-primary font-semibold">mobile development</strong>, utilizing technologies like ReactJS, Next.js, Node.js, Typescript and JavaScript. I have worked on diverse projects that span both <strong className="text-primary font-semibold">front-end and back-end development</strong>. My journey in the tech world has been driven by curiosity and a passion for building efficient and user-centered applications.
           </p>
         </div>
 
